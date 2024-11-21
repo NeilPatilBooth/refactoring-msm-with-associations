6b442b34e633cc9fad0e3489b15da0bfb693f8e6
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-11-21 12:11:36 +0000

    stuck - looked at solution

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,9 +20,11 @@ class Movie < ApplicationRecord
   #  x = self.director_id
   #  return Director.where({ :id => x}).first
   #end
-  belongs_to(:director, class_name: "Director", foreign_key: "director_id") # :Symbol-can be anything and defines the new method, class_name: Table, foreign_key: String containing name of foreign key column
 
+  belongs_to(:director, class_name: "Director", foreign_key: "director_id") # :Symbol-can be anything and defines the new method, class_name: Table, foreign_key: String containing name of foreign key column
+  
   has_many(:characters, class_name: "Character", foreign_key:"movie_id")
+  has_many(:cast, :through => :characters, :source => :actor)
 
 
 end
